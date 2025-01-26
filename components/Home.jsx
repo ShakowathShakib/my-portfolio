d5a11dabf712cfc2079d54f01ebf691469e4ce24
Mazharul Islam <mazharulislam3569@gmail.com>
2025-01-26 22:06:31 +0600

    add about and home page

---
+++ components/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+     return <div>
+          <h1>Welcome message</h1>
+          <div>
+              Write Your vision and mission
+          </div>
+     </div>
+     }
+    
---
+++ src/about.jsx
@@ -0,0 +1,11 @@
+import myImg from './assets/myImg.jpeg'
+
+export default function About() {
+     return (
+     <>
+     <h2>Your name</h2>
+     <img width={200} src={myImg} alt="" />
+     </>
+     );
+    }
+    
