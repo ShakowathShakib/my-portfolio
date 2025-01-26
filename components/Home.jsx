fb3179e30ff7f8913ce9877130ba5a8b560adf7c
ShakowathShakib <shshakib44@gmail.com>
2025-01-27 01:10:37 +0600

    add home page

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,8 +1,12 @@
 export default function Home() {
      return <div>
-          <h1>Welcome message</h1>
+          <h1>Welcome to my portfolio</h1>
           <div>
-              Write Your vision and mission
+               <h2>Vision Statement:</h2>
+         <p> To innovate and build transformative software solutions that empower individuals and businesses to achieve their full potential, fostering a future where technology enhances every aspect of life and work.</p>
+
+         <h2>Mision Statement:</h2>
+         <p> Our mission is to design, develop, and deliver high-quality software solutions that are user-centric, scalable, and secure. We are committed to continuous learning, collaboration, and improvement, with the goal of creating impactful products that solve real-world problems and drive technological advancement.</p>
           </div>
      </div>
      }
--- src/about.jsx
+++ src/about.jsx
@@ -1,10 +1,7 @@
-import myImg from './assets/myImg.jpeg'
-
 export default function About() {
      return (
      <>
-     <h2>Your name</h2>
-     <img width={200} src={myImg} alt="" />
+     <p>About</p>
      </>
      );
     }
