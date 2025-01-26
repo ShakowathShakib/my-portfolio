478635e56f5393afed67fbd6d12e90af80f519f0
ShakowathShakib <shshakib44@gmail.com>
2025-01-27 01:58:53 +0600

    add logo

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom';
+import logo from "../src/assets/s_logo-removebg-preview.png"
+
 export default function Layout() {
  return (
  <>
- <h1 className='header'>My Portfolio</h1>
+<div style={{display:"flex"}}>
+<img width={100} src={logo} alt="logo" />
+<h1 className='header'>My Portfolio</h1>
+</div>
  <nav>
  <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/education">Education</Link>| <Link to="/project">Project</Link>| <Link to="/contact">Contact</Link>
  </nav>
--- src/contact.jsx
+++ src/contact.jsx
@@ -1,8 +1,26 @@
 export default function Contact() {
      return (
-     <>
-     <p>Contact</p>
-     </>
+     <div>
+          <h2>Contact Us</h2>
+          <form className="contact-form">
+          <label htmlFor="name">First Name</label>
+    <input type="text" id="name" name="name" placeholder="Enter your name" required/>
+    
+          <label htmlFor="name">Last Name</label>
+    <input type="text" id="name" name="name" placeholder="Enter your name" required/>
+
+    <label htmlFor="email">Your Email</label>
+    <input type="email" id="email" name="email" placeholder="Enter your email" required/>
+
+    <label htmlFor="message">Your Message</label>
+    <textarea id="message" name="message" placeholder="Enter your message" required></textarea>
+
+    <button type="submit">Send Message</button>
+          </form>
+     
+    
+    
+  
+     </div>
      );
      }
-    
